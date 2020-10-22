70d0156c0415b467493e6574ab6c3e90b933bc32
mwinamijr <mwinamijr@gmail.com>
2020-10-22 18:10:40 +0300

    added footer and its contents

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 class BaseLayout extends Component {
     render() {
         return (
             <div>
                 <Navbar />
+                <Footer />
             </div>
         )
     }
