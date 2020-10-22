0240f27c281c9bf10e8fd9f4f77ef214931f2057
mwinamijr <mwinamijr@gmail.com>
2020-10-22 15:00:30 +0300

    added rest-api and rest-auth urls

--- ajira/posts/models.py
+++ ajira/posts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Post(models.Model):
-    photo = models.ImageField(upload_to="my_images")
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    photo = models.ImageField(upload_to="my_images", null=True, blank= True)
+    title = models.CharField(max_length=100, null=True, blank= True)
+    description = models.TextField(null=True, blank= True)
 
     def __str__(self):
         return self.title
