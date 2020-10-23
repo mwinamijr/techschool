59e4bf6398a57c6520221395c15912a8b2824890
mwinamijr <mwinamijr@gmail.com>
2020-10-23 11:53:32 +0300

    added routes to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import BaseLayout from "./containers/Layout";
+import BaseRouter from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <BaseLayout />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container ">
+          <BaseLayout>
+            <BaseRouter />
+          </BaseLayout>
+        </div>
+      </div>
+    </Router>
   );
 }
 
