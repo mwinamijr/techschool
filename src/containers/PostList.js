89fe005199ef0ad9ed0ec9a81bcc0fb6bc58ec5c
mwinamijr <mwinamijr@gmail.com>
2020-10-23 12:37:45 +0300

    modified post list items

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -1,121 +1,150 @@
 import React from "react";
-import {  MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
 
 const PostList = () => {
   return (
-    <section className="my-5">
+    <MDBCard className="my-5 px-5 pb-5">
+      <MDBCardBody>
         <h2 className="h1-responsive font-weight-bold text-center my-5">
-          Our best projects
+          Recent posts
         </h2>
-        <p className="grey-text w-responsive text-center mx-auto mb-5">
+        <p className="text-center w-responsive mx-auto mb-5">
           Duis aute irure dolor in reprehenderit in voluptate velit esse
           cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
           cupidatat non proident, sunt in culpa qui officia deserunt mollit
-          est laborum.
+          anim id est laborum.
         </p>
-
         <MDBRow>
-          <MDBCol lg="5" className="mb-lg-0 mb-5">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Others/images/83.jpg"
-              alt=""
-              className="img-fluid rounded z-depth-1"
-            />
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
           </MDBCol>
           <MDBCol lg="7">
-            <MDBRow className="mb-3">
-              <MDBCol md="1" size="2">
-                <MDBIcon far icon="chart-bar" size="2x" className="indigo-text" />
-              </MDBCol>
-              <MDBCol md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Marketing</h5>
-                <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing.
-                  Reprehenderit maiores nam, aperiam minima elit assumenda
-                  voluptate velit.
-                </p>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="mb-3">
-              <MDBCol md="1" size="2">
-                <MDBIcon icon="music" size="2x" className="pink-text" />
-              </MDBCol>
-              <MDBCol md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Entertainment</h5>
-                <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing.
-                  Reprehenderit maiores nam, aperiam minima elit assumenda
-                  voluptate velit.
-                </p>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="mb-3">
-              <MDBCol md="1" size="2">
-                <MDBIcon far icon="smile" size="2x" className="blue-text" />
-              </MDBCol>
-              <MDBCol md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Communication</h5>
-                <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing.
-                  Reprehenderit maiores nam, aperiam minima elit assumenda
-                  voluptate velit.
-                </p>
-              </MDBCol>
-            </MDBRow>
+            <a href="#!" className="green-text">
+              <h6 className="font-weight-bold mb-3">
+                <MDBIcon icon="utensils" className="pr-2" />
+                Food
+              </h6>
+            </a>
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Title of the news</strong>
+            </h3>
+            <p>
+              Nam libero tempore, cum soluta nobis est eligendi optio cumque
+              nihil impedit quo minus id quod maxime placeat facere possimus,
+              omnis voluptas assumenda est, omnis dolor repellendus et aut
+              officiis debitis.
+            </p>
+            <p>
+              by
+              <a href="#!">
+                <strong>Carine Fox</strong>
+              </a>
+              , 19/08/2018
+            </p>
+            <MDBBtn color="success" size="md" className="waves-light ">
+              Read more
+            </MDBBtn>
           </MDBCol>
         </MDBRow>
         <hr className="my-5" />
         <MDBRow>
           <MDBCol lg="7">
-            <MDBRow className="mb-3">
-              <MDBCol md="1" size="2">
-                <MDBIcon icon="book" size="2x" className="cyan-text" />
-              </MDBCol>
-              <MDBCol md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Education</h5>
-                <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing.
-                  Reprehenderit maiores nam, aperiam minima elit assumenda
-                  voluptate velit.
-                </p>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="mb-3">
-              <MDBCol md="1" size="2">
-                <MDBIcon icon="code" size="2x" className="red-text" />
-              </MDBCol>
-              <MDBCol md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Technology</h5>
-                <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing.
-                  Reprehenderit maiores nam, aperiam minima elit assumenda
-                  voluptate velit.
-                </p>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="mb-3">
-              <MDBCol md="1" size="2">
-                <MDBIcon far icon="money-bill-alt" size="2x" className="deep-purple-text" />
-              </MDBCol>
-              <MDBCol md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Finance</h5>
-                <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing.
-                  Reprehenderit maiores nam, aperiam minima elit assumenda
-                  voluptate velit.
-                </p>
-              </MDBCol>
-            </MDBRow>
+            <a href="#!" className="pink-text">
+              <h6 className="font-weight-bold mb-3">
+                <MDBIcon icon="image" className="pr-2" />
+                Lifestyle
+              </h6>
+            </a>
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Title of the news</strong>
+            </h3>
+            <p>
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate
+              non provident.
+            </p>
+            <p>
+              by
+              <a href="#!">
+                <strong>Carine Fox</strong>
+              </a>
+              , 14/08/2018
+            </p>
+            <MDBBtn
+              color="pink"
+              size="md"
+              className="mb-lg-0 mb-4 waves-light"
+            >
+              Read more
+            </MDBBtn>
           </MDBCol>
           <MDBCol lg="5">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Others/images/82.jpg"
-              alt=""
-              className="img-fluid rounded z-depth-1"
-            />
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="https://mdbootstrap.com/img/Photos/Others/img%20(34).jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
           </MDBCol>
         </MDBRow>
-      </section>
+        <hr className="my-5" />
+        <MDBRow>
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="https://mdbootstrap.com/img/Photos/Others/img (28).jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
+          </MDBCol>
+          <MDBCol lg="7">
+            <a href="#!" className="indigo-text">
+              <h6 className="font-weight-bold mb-3">
+                <MDBIcon icon="suitcase" className="pr-2" />
+                Food
+              </h6>
+            </a>
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Title of the news</strong>
+            </h3>
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+              aut fugit, sed quia consequuntur magni dolores eos qui ratione
+              voluptatem sequi nesciunt. Neque porro qui dolorem ipsum quia
+              sit amet.
+            </p>
+            <p>
+              by
+              <a href="#!">
+                <strong>Carine Fox</strong>
+              </a>
+              , 11/08/2018
+            </p>
+            <MDBBtn color="indigo" size="md" className="waves-light ">
+              Read more
+            </MDBBtn>
+          </MDBCol>
+        </MDBRow>
+      </MDBCardBody>
+    </MDBCard>
   );
 }
 
