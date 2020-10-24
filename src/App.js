cd498b687f1243d383a8c281f97de01070136a0c
mwinamijr <mwinamijr@gmail.com>
2020-10-24 14:29:09 +0300

    added checkAutoSignUp action in redux actions

--- src/App.js
+++ src/App.js
@@ -2,21 +2,43 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { connect } from 'react-redux'
+
+import * as actions from './store/actions/auth';
+
 import BaseLayout from "./containers/Layout";
 import BaseRouter from './routes';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="container ">
-          <BaseLayout>
-            <BaseRouter />
-          </BaseLayout>
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignUp();
+  }
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <div className="container ">
+            <BaseLayout {...this.props}>
+              <BaseRouter />
+            </BaseLayout>
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return{
+    isAuthenticated: state.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignUp: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -29,9 +29,9 @@ export const logout = ()  => {
     }
 }
 
-checkAuthTimeOut = expirationTime =>{
+export const checkAuthTimeOut = expirationTime =>{
     return dispatch => {
-        setTimeOut(() => {
+        setTimeout(() => {
             dispatch(logout());
         }, expirationTime * 1000)
     }
@@ -40,29 +40,29 @@ checkAuthTimeOut = expirationTime =>{
 export const authLogin = (username, password) => {
     return dispatch => {
         dispatch(authStart());
-        axios.post("http:127.0.0.1:8000/rest-auth/login/", {
+        axios.post("http://127.0.0.1:8000/rest-auth/login/", {
             username: username,
             password: password
         })
         .then(res => {
             const token = res.data.key;
             const expirationDate = new Date().getTime() + 3600
-            localStorage.setItem('token': token);
-            localStorage.setItem('expirationDate': expirationDate);
+            localStorage.setItem('token', token);
+            localStorage.setItem('expirationDate', expirationDate);
             dispatch(authSuccess(token));
             dispatch(checkAuthTimeOut(3600));
-        }).
-        catch(error => {
+        })
+        .catch(error => {
             dispatch(authFail(error));
         })
     }
 }
 
 
-export const authSignup = (username, password) => {
+export const authSignup = (username, email, password1, password2) => {
     return dispatch => {
         dispatch(authStart());
-        axios.post("http:127.0.0.1:8000/rest-auth/registration/", {
+        axios.post("http://127.0.0.1:8000/rest-auth/registration/", {
             username: username,
             email: email,
             password1: password1,
@@ -71,13 +71,31 @@ export const authSignup = (username, password) => {
         .then(res => {
             const token = res.data.key;
             const expirationDate = new Date().getTime() + 3600
-            localStorage.setItem('token': token);
-            localStorage.setItem('expirationDate': expirationDate);
+            localStorage.setItem('token', token);
+            localStorage.setItem('expirationDate', expirationDate);
             dispatch(authSuccess(token));
             dispatch(checkAuthTimeOut(3600));
-        }).
-        catch(error => {
+        })
+        .catch(error => {
             dispatch(authFail(error));
         })
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (token === undefined) {
+            dispatch(logout());
+        }
+        else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate <= new Date() ) {
+                dispatch(logout());
+            } else {
+                dispatch(authSuccess());
+                dispatch(checkAuthTimeOut( (expirationDate.getTime() - new Date().getTime()) / 1000));
+            }
+        }
     }
 }
