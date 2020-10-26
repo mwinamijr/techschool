177dd72807f95cca1124c9d6a628148c531f076d
mwinamijr <mwinamijr@gmail.com>
2020-10-26 10:27:48 +0300

    changed rest_framework authentication

--- ajira/ajira/settings.py
+++ ajira/ajira/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
