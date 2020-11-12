2ed40834871036b5c96ecef43809682aece053dc
mwinamijr <mwinamijr@gmail.com>
2020-11-12 15:51:45 +0300

    changed contents of post list and post details

--- src/containers/PostDetails.js
+++ src/containers/PostDetails.js
@@ -1,59 +1,67 @@
 import React from "react";
-import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
+import axios from "axios";
+import { connect } from "react-redux";
+import { Button, Card } from "antd";
+import CustomForm from "../components/Form";
 
-const PostList = () => {
-  return (
-    <MDBCard className="my-5 px-5 pb-5">
-      <MDBCardBody>
-        <h2 className="h1-responsive font-weight-bold text-center my-5">
-          Recent posts
-        </h2>
-        <MDBRow>
-          <MDBCol lg="5">
-            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
-              <img
-                className="img-fluid"
-                src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
-                alt=""
-              />
-              <a href="#!">
-                <MDBMask overlay="white-slight" />
-              </a>
-            </MDBView>
-          </MDBCol>
-          <MDBCol lg="7">
-            <a href="#!" className="green-text">
-              <h6 className="font-weight-bold mb-3">
-                <MDBIcon icon="utensils" className="pr-2" />
-                Food
-              </h6>
-            </a>
-            <h3 className="font-weight-bold mb-3 p-0">
-              <strong>Title of the news</strong>
-            </h3>
-            <p>
-              Nam libero tempore, cum soluta nobis est eligendi optio cumque
-              nihil impedit quo minus id quod maxime placeat facere possimus,
-              omnis voluptas assumenda est, omnis dolor repellendus et aut
-              officiis debitis.
-            </p>
-            <p>
-              by
-              <a href="#!">
-                <strong>Carine Fox</strong>
-              </a>
-              , 19/08/2018
-            </p>
-            <MDBBtn color="success" size="md" className="waves-light ">
-              Read more
-            </MDBBtn>
-          </MDBCol>
-        </MDBRow>
-        <hr className="my-5" />
-        
-      </MDBCardBody>
-    </MDBCard>
-  );
+
+class PostDetail extends React.Component {
+  state = {
+    post: {}
+  };
+
+  componentDidMount() {
+    const postID = this.props.match.params.postID;
+    axios.get(`http://127.0.0.1:8000/posts/${postID}/`).then(res => {
+      this.setState({
+        post: res.data
+      });
+    console.log(res.data)
+    });
+  }
+
+  handleDelete = event => {
+    event.preventDefault();
+    const postID = this.props.match.params.postID;
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Token ${this.props.token}`
+    };
+    axios.delete(`http://127.0.0.1:8000/posts/${postID}/delete/`)
+    .then(res => {
+      if (res.status === 204) {
+        this.props.history.push(`/`);
+      }
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <Card title={this.state.post.title}>
+          <p> {this.state.post.description} </p>
+        </Card>
+        <CustomForm
+          {...this.props}
+          token={this.props.token}
+          requestType="put"
+          postID={this.props.match.params.postID}
+          btnText="Update"
+        />
+        <form onSubmit={this.handleDelete}>
+          <Button type="danger" htmlType="submit">
+            Delete
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default PostList;
+const mapStateToProps = state => {
+  return {
+    token: state.token
+  };
+};
+
+export default connect(mapStateToProps)(PostDetail);
--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,115 +1,41 @@
 import React from "react";
-import { connect } from "react-redux";
-import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
+import axios from "axios";
+import Posts from "../components/Post";
+import CustomForm from "../components/Form";
 
-import * as actions from "../store/actions/post";
 
 class PostList extends React.Component {
+  state = {
+    posts: []
+  };
+
+  fetchPosts = () => {
+    axios.get("http://127.0.0.1:8000/posts/").then(res => {
+      this.setState({
+        posts: res.data
+      });
+    });
+  }
+
   componentDidMount() {
-    if (this.props.token !== undefined && this.props.token !== null) {
-      this.props.getPosts(this.props.token);
-    }
+    this.fetchPosts();
   }
 
   componentWillReceiveProps(newProps) {
-    if (newProps.token !== this.props.token) {
-      if (newProps.token !== undefined && newProps.token !== null) {
-        this.props.getPosts(newProps.token);
-      }
+    if (newProps.token) {
+      this.fetchPosts();      
     }
   }
-  
-  renderItem = this.props.posts.map( (post) => {
 
+  render() {
     return (
-      <span>
-        <MDBRow>
-          <MDBCol lg="7">
-            <a href="#!" className="pink-text">
-              <h6 className="font-weight-bold mb-3">
-                <MDBIcon icon="image" className="pr-2" />
-                Lifestyle
-              </h6>
-            </a>
-            <h3 className="font-weight-bold mb-3 p-0">
-              <strong>Title of the news</strong>
-            </h3>
-            <p>
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              blanditiis praesentium voluptatum deleniti atque corrupti quos
-              dolores et quas molestias excepturi sint occaecati cupiditate
-              non provident.
-            </p>
-            <p>
-              by
-              <a href="#!">
-                <strong>Carine Fox</strong>
-              </a>
-              , 14/08/2018
-            </p>
-            <MDBBtn
-              color="pink"
-              size="md"
-              className="mb-lg-0 mb-4 waves-light"
-            >
-              Read more
-            </MDBBtn>
-          </MDBCol>
-          <MDBCol lg="5">
-            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
-              <img
-                className="img-fluid"
-                src="https://mdbootstrap.com/img/Photos/Others/img%20(34).jpg"
-                alt=""
-              />
-              <a href="#!">
-                <MDBMask overlay="white-slight" />
-              </a>
-            </MDBView>
-          </MDBCol>
-        </MDBRow>
-        <hr className="my-5" />
-      </span>
-    );
-  });
-
-  render(){
-    return (
-      <MDBCard className="my-5 px-5 pb-5">
-        <MDBCardBody>
-          <h2 className="h1-responsive font-weight-bold text-center my-5">
-            Recent posts
-          </h2>
-          <p className="text-center w-responsive mx-auto mb-5">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
-          {this.renderItem}
-        </MDBCardBody>
-      </MDBCard>
+      <div>
+        <Posts data={this.state.posts} /> <br />
+        <h2> Create an article </h2>
+        <CustomForm requestType="post" articleID={null} btnText="Create" />
+      </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-
-  console.log(state)
-  return {
-    token: state.auth.token,
-    posts: state.posts,
-    loading: state.posts.loading
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    getPosts: token => dispatch(actions.getPosts(token))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PostList);
+export default PostList;
