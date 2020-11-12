ef095b780808e2a8b8795cba48fb2ac43763b0f1
mwinamijr <mwinamijr@gmail.com>
2020-11-12 15:51:12 +0300

    modified layout view

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,192 +1,65 @@
-import React from "react";
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
 
-import  { Link, NavLink } from 'react-router-dom';
+const { Header, Content, Footer } = Layout;
 
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import HomeIcon from '@material-ui/icons/Home';
-
-import { AppBar, Toolbar, IconButton, Typography, Button, MenuItem, Menu } from '@material-ui/core';
-
-import {useStyles} from './css/Navbar';
-
-
-//import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
-const BaseLayout = (props) => {
-
-	const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  console.log(props);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-    {
-        props.isAuthenticated ?
-      <span>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="logout"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <ExitToAppIcon />
-        </IconButton>
-        <p>Logout</p>
-      </MenuItem>
-      </span>
-
-      :
-      <span>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <NavLink to="/login">
-          <IconButton
-            aria-label="logout"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <ExitToAppIcon />
-          </IconButton>
-          <p>Login</p>
-          </NavLink>
-        </MenuItem>
-      </span>
+class CustomLayout extends React.Component {
+    render() {
+        return (
+            <Layout className="layout">
+                <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+    
+                {
+                    this.props.isAuthenticated ?
+    
+                    <Menu.Item key="2" onClick={this.props.logout}>
+                        Logout
+                    </Menu.Item>
+    
+                    :
+    
+                    <Menu.Item key="2">
+                        <Link to="/login">Login</Link>
+                    </Menu.Item>
+                }
+    
+                    <Menu.Item key="1">
+                        <Link to="/posts">Posts</Link>
+                    </Menu.Item>
+                    
+                </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+                    <Breadcrumb.Item><Link to="/posts">List</Link></Breadcrumb.Item>
+                </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                        {this.props.children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2016 Created by Ant UED
+                </Footer>
+            </Layout>
+        );
     }
-
-    </Menu>
-  );
-
-  return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Ajira
-          </Typography>
-
-          <div>
-          <IconButton>
-            <Link to="/">
-              <HomeIcon />
-            </Link>
-          </IconButton>
-          
-          <Button color="inherit"><Link to="/posts">Posts</Link></Button>
-          </div>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            
-            {
-              props.isAuthenticated ?
-              <span>
-                <IconButton>
-                  <AccountCircle />
-                  <p>Profile</p>
-                </IconButton>
-                <Button color="inherit">
-                  <NavLink to="/logout">
-                  Logout
-                  </NavLink>
-                </Button>
-              </span>
-              :
-                <Button color="inherit">
-                  <NavLink to="/login">
-                  Login
-                  </NavLink>
-                </Button>
-
-            }
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-
-      { props.children }
-      <Footer />
-    </div>
-  );
 }
 
-export default BaseLayout;
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(actions.logout()) 
+    }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(CustomLayout));
