74680521816880a01127000346103a9189339dd7
mwinamijr <mwinamijr@gmail.com>
2024-09-06 15:16:00 +0300

    created user login slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,14 +1,74 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+  removeUserFromLocalStorage,
+} from "../utils/localStorage";
+
+const baseUrl = "http://127.0.0.1:8000";
 
 const initialState = {
   loading: false,
   error: null,
-  user: null,
+  user: getUserFromLocalStorage(),
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
+  reducers: {
+    logout: (state, { payload }) => {
+      state.user = null;
+      removeUserFromLocalStorage();
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        const user = payload;
+        state.loading = false;
+        state.user = user;
+        addUserToLocalStorage(user);
+      })
+      .addCase(loginUser.rejected, (state, { payload }) => {
+        const error = payload;
+        console.log(error);
+        state.error = error;
+      });
+  },
 });
 
+export const loginUser = createAsyncThunk(
+  "user/login",
+  async (user, thunkAPI) => {
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `${baseUrl}/api/users/login/`,
+        user,
+        config
+      );
+      return data;
+    } catch (error) {
+      return error.message;
+    }
+  }
+);
+
+export const registerUser = createAsyncThunk(
+  "/user/register",
+  async (user, thunkAPI) => {
+    console.log(`Register user: ${user}`);
+  }
+);
+
+export const { logout } = userSlice.actions;
 export default userSlice.reducer;
