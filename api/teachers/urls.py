6e30ef8b25490a66277b34b29b473e038dfff019
mwinamijr <mwinamijr@gmail.com>
2024-09-10 16:38:24 +0300

    removed teacher and student model, serializer, viwes and urls

--- api/teachers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users.views import (
-    TeacherListView,
-)
-
-urlpatterns = [
-    path("", TeacherListView.as_view(), name="teacher-list"),
-]
--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-from .models import CustomUser, Student, Teacher
+from .models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -35,7 +35,6 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserSerializerWithToken(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
-    user_type = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = CustomUser
@@ -46,31 +45,11 @@ class UserSerializerWithToken(UserSerializer):
             "middle_name",
             "last_name",
             "is_admin",
-            "user_type",
+            "is_teacher",
+            "is_student",
             "token",
         ]
 
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
-
-    def get_user_type(self, obj):
-        serializer_data = UserSerializer(obj).data
-        is_teacher = serializer_data.get("is_teacher")
-        is_student = serializer_data.get("is_student")
-        if is_teacher:
-            return {"is_teacher": is_teacher}
-        else:
-            return {"is_student": is_student}
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = "__all__"
-
-
-class TeacherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Teacher
-        fields = "__all__"
