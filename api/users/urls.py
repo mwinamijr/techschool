f258706bac05d28d4c105ea7d181824d990377b6
mwinamijr <mwinamijr@gmail.com>
2024-09-10 19:49:25 +0300

    added new user views to the urls

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from users.views import (
     MyTokenObtainPairView,
-    UserListView,
-    UserDetailView,
+    registerUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
 )
 
 urlpatterns = [
     path("login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("", UserListView.as_view(), name="user-list"),
-    path("<str:pk>/", UserDetailView.as_view(), name="user-details"),
+    path("register/", registerUser, name="register"),
+    path("profile/", getUserProfile, name="users-profile"),
+    path("profile/update/", updateUserProfile, name="user-profile-update"),
+    path("", getUsers, name="users"),
+    path("<str:pk>/", getUserById, name="user"),
+    path("update/<str:pk>/", updateUser, name="user-update"),
+    path("delete/<str:pk>/", deleteUser, name="user-delete"),
 ]
--- techschool/urls.py
+++ techschool/urls.py
@@ -26,8 +26,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html")),
     path("api/users/", include("api.users.urls")),
-    path("api/students/", include("api.students.urls")),
-    path("api/teachers/", include("api.teachers.urls")),
     path("__debug__/", include(debug_toolbar.urls)),
 ]
 
