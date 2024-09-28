ec2d22c9a496f0ef78c6bc6cd4b0439d1c0c6bf3
mwinamijr <mwinamijr@gmail.com>
2024-09-29 00:19:25 +0300

    removed all files

--- ajira/posts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    title = models.CharField(max_length=100, null=True, blank= True)
-    description = models.TextField(null=True, blank= True)
-    photo = models.ImageField(upload_to="my_images", null=True, blank= True)
-
-    def __str__(self):
-        return self.title
